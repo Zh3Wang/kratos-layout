c8d07266d5501762edc3fdf48f5e1c18f75c6212
wangzhe <wangzhe@bell.ai>
2021-07-23 17:48:09 +0800

    目录调整

--- service/internal/pkg/middleware/auth/error.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "github.com/go-kratos/kratos/v2/errors"
-
-var (
-	errAuthFail      = errors.New(401, "AUTHENTICATION FAILED", "Missing token or token incorrect")
-	errAuthTypeError = errors.New(401, "AUTHENTICATION FAILED", "token type error")
-)
